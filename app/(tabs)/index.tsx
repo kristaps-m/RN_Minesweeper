5442d4ed11f6c5178403e47a57009de4115f74eb
Kristaps-M <kristapsmitins@gmail.com>
2024-01-02 16:19:43 +0200

    init

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,13 +1,42 @@
-import { StyleSheet } from 'react-native';
+import { Pressable, StyleSheet } from "react-native";
 
-import EditScreenInfo from '../../components/EditScreenInfo';
-import { Text, View } from '../../components/Themed';
+import EditScreenInfo from "../../components/EditScreenInfo";
+import { Text, View } from "../../components/Themed";
+import { useEffect, useState } from "react";
 
 export default function TabOneScreen() {
+  const [theN, setTheN] = useState(0);
+  function increaseTheNHandler() {
+    let tempNum = theN;
+    tempNum++;
+    setTheN(tempNum);
+  }
+
+  function decreaseTheNHandler() {
+    let tempNum = theN;
+    tempNum--;
+    setTheN(tempNum);
+  }
+
+  useEffect(() => {});
+
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Tab One</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
+      <View style={{ flexDirection: "row" }}>
+        <Pressable onPress={decreaseTheNHandler}>
+          <Text>--</Text>
+        </Pressable>
+        <Text>{theN}</Text>
+        <Pressable onPress={increaseTheNHandler}>
+          <Text>++</Text>
+        </Pressable>
+      </View>
+      <View
+        style={styles.separator}
+        lightColor="#eee"
+        darkColor="rgba(255,255,255,0.1)"
+      />
       <EditScreenInfo path="app/(tabs)/index.tsx" />
     </View>
   );
@@ -16,16 +45,16 @@ export default function TabOneScreen() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
+    justifyContent: "center",
   },
   title: {
     fontSize: 20,
-    fontWeight: 'bold',
+    fontWeight: "bold",
   },
   separator: {
     marginVertical: 30,
     height: 1,
-    width: '80%',
+    width: "80%",
   },
 });
