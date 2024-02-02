d64a4a8eadf24f5be78f4af48fba3b0a77d96db9
Kristaps-M <kristapsmitins@gmail.com>
2024-02-03 00:11:16 +0200

    snake works as 2d grid and Grid Text?

--- app/(tabs)/two.tsx
+++ app/(tabs)/two.tsx
@@ -4,22 +4,36 @@ import EditScreenInfo2 from "../../components/EditScreenInfo2";
 import { Text, View } from "../../components/Themed";
 import { useEffect, useState } from "react";
 
+interface SnakeCell {
+  x: number;
+  y: number;
+}
+
 export default function TabTwoScreen() {
   const listOfTestCh = ["#", "@", "%", "+", "_", "X"];
-  const [snakeHead, setSnakeHead] = useState({ x: 0, y: 0 });
+  // const [snakeHead, setSnakeHead] = useState({ x: 0, y: 0 });
+  const [snakeHead, setSnakeHead] = useState<SnakeCell[]>([
+    { x: 0, y: 0 },
+    { x: 1, y: 0 },
+    { x: 2, y: 0 },
+  ]);
   const [gameRunning, setGameRunning] = useState(true);
   const [xDir, setXDir] = useState(0);
   const [xVelocity, setXVelocity] = useState(0);
   const [yDir, setYDir] = useState(0);
   const [snakeField, setSnakeField] = useState([
-    ["#", ".", ".", ".", ".", ".", ".", "."],
-    [".", ".", ".", ".", ".", ".", ".", "."],
-    [".", ".", ".", ".", ".", ".", ".", "."],
-    [".", ".", ".", ".", ".", ".", ".", "."],
-    [".", ".", ".", ".", ".", ".", ".", "."],
-    [".", ".", ".", ".", ".", ".", ".", "."],
-    [".", ".", ".", ".", ".", ".", ".", "."],
-    [".", ".", ".", ".", ".", ".", ".", "."],
+    [" ", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", "."],
+    [".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", "."],
+    [".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", "."],
+    [".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", "."],
+    [".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", "."],
+    [".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", "."],
+    [".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", "."],
+    [".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", "."],
+    [".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", "."],
+    [".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", "."],
+    [".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", "."],
+    [".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", "."],
   ]);
 
   let c = 0;
@@ -32,18 +46,58 @@ export default function TabTwoScreen() {
   //   setXDir(-1);
   // }
   let gameTimerId: any;
-  let gameTimerId2: any;
+  // let gameTimerId2: any;
+  function drawField() {
+    let tempSnakeField = [...snakeField];
+    const snakeLenght = snakeHead.length;
+    // let tempSnakeHead: SnakeCell[] = snakeHead;
+    let tempSnakeHead = [...snakeHead][snakeLenght - 1];
+    let newSnakeHead = {
+      x: (tempSnakeHead.x += xDir),
+      y: (tempSnakeHead.y += yDir),
+    };
+    snakeHead.push(newSnakeHead);
+    // tempSnakeField[snakeHead[0].x][snakeHead[0].y] = ".";
+    // setSnakeField(tempSnakeField);
+    for (
+      let index = 0;
+      index < snakeHead.slice(0, snakeHead.length).length;
+      index++
+    ) {
+      tempSnakeField[snakeHead[index].x][snakeHead[index].y] = "#";
+    }
+    setSnakeField(tempSnakeField);
+    snakeHead.shift();
+  }
+
+  function resetFieldLook() {
+    let tempSnakeField = [...snakeField];
+    // for (let index = 0; index < snakeHead.slice(1, -1).length; index++) {
+    //   tempSnakeField[snakeHead[index].x][snakeHead[index].y] = "#";
+    // }
+    for (let row = 0; row < tempSnakeField.length; row++) {
+      for (let col = 0; col < tempSnakeField[0].length; col++) {
+        tempSnakeField[row][col] = " ";
+      }
+    }
+    setSnakeField(tempSnakeField);
+  }
 
   useEffect(() => {
     // if (gameRunning) {
 
     gameTimerId = setInterval(() => {
       // if (xDir !== 0) {
-      console.log(`Before ${c}`);
+      // console.log(`Before ${c}`);
+      // console.log(snakeHead);
+      // let tempSnakeField = [...snakeField];
+      // tempSnakeField[snakeHead.x][snakeHead.y] = ".";
+      // setSnakeField(tempSnakeField);
+      resetFieldLook();
+      drawField();
+
+      // snakeHead.shift();
       console.log(snakeHead);
-      let tempSnakeField = [...snakeField];
-      tempSnakeField[snakeHead.x][snakeHead.y] = ".";
-      setSnakeField(tempSnakeField);
       /// SET ALL
       // let tempSnakeH = snakeHead;
       // tempSnakeH.x += xDir;
@@ -51,133 +105,70 @@ export default function TabTwoScreen() {
       // console.log(snakeField);
       // console.log(xDir);
       // }
-    }, 1500);
-    // c++;
-    // console.log("TEST");
-
-    gameTimerId2 = setInterval(() => {
-      // if (xDir !== 0) {
-
-      /// SET ALL
-      // let tempSnakeH = snakeHead;
-      // tempSnakeH.x += xDir;
-      setSnakeHead({ x: snakeHead.x + xDir, y: snakeHead.y + yDir });
-      let tempSnakeField2 = [...snakeField];
-      tempSnakeField2[snakeHead.x][snakeHead.y] = "#";
-
-      setSnakeField(tempSnakeField2);
-      console.log(`After ${c}`);
-      console.log(snakeHead);
-      // console.log(snakeField);
-      // console.log(xDir);
-      // }
-    }, 1500);
+    }, 1000);
     // c++;
     // console.log("TEST");
 
     return () => {
       clearInterval(gameTimerId);
-      clearInterval(gameTimerId2);
     };
     // }
-  }, [snakeHead, snakeField, c]);
+  }, [snakeField, xDir, yDir]);
   // console.log(snakeHead);
-  // function startGameHandler() {
-  //   setInterval(() => {
-  //     if (xDir !== 0) {
-  //       let tempSnakeField = [...snakeField];
-  //       tempSnakeField[snakeHead.y][snakeHead.x] = ".";
-  //       /// SET ALL
-  //       let tempSnakeH = snakeHead;
-  //       tempSnakeH.x += xDir;
-  //       // let x = xDir;
-  //       // let y = 0;
-  //       setSnakeHead(tempSnakeH);
-  //       tempSnakeField[snakeHead.y][snakeHead.x] = "#";
-
-  //       setSnakeField(tempSnakeField);
-  //       console.log(snakeHead);
-  //       console.log(snakeField);
-  //       console.log(xDir);
-  //     }
-  //     // else if(){
-
-  //     // }
-  //   }, 7000);
-  // }
-
-  // snakeField[0][0] = "X";
-
-  // function startGameHandler() {
-  //   let testChLen = 0;
-  //   let row = 0;
-  //   let col = 0;
-  //   setInterval(() => {
-  //     let tempField = [...snakeField];
-  //     tempField[row][col] = listOfTestCh[testChLen];
-  //     console.log(row, col);
-  //     setSnakeField(tempField);
-  //     col++;
-  //     if (col === snakeField[0].length) {
-  //       row++;
-  //       col = 0;
-  //     }
-  //     if (row === snakeField.length) {
-  //       row = 0;
-  //       testChLen++;
-  //     }
-  //     if (testChLen === listOfTestCh.length) {
-  //       testChLen = 0;
-  //     }
-  //     // setInterval(() => 5000);
-  //   }, 200);
-  //   // for (let row = 0; row < snakeField.length; row++) {
-  //   //   for (let col = 0; col < snakeField[0].length; col++) {
-  //   //     let tempField = [...snakeField];
-  //   //     tempField[row][col] = "#";
-  //   //     setSnakeField(tempField);
-  //   //     setInterval(() => 5000);
-  //   //   }
-  //   // }
-  // }
-
-  // useEffect(() => {
-  //   for (let row = 0; row < snakeField.length; row++) {
-  //     for (let col = 0; col < snakeField[0].length; col++) {
-  //       let tempField = [...snakeField];
-  //       tempField[row][col] = "#";
-  //       setSnakeField(tempField);
-  //       setTimeout(() => 5000);
-  //     }
-  //   }
-
-  //   return () => {
-  //     clearTimeout(500);
-  //   };
-  // }, []);
 
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Tab Two</Text>
       <Text>
-        {xDir}, sh.x={snakeHead.x}, sh.y{snakeHead.y}
+        {xDir}, sh.x={snakeHead[0].x}, sh.y{snakeHead[0].y}
       </Text>
       <View
         style={styles.separator}
         lightColor="#eee"
         darkColor="rgba(255,255,255,0.1)"
       />
+      <Pressable
+        style={styles.directionButtons}
+        onPress={() => {
+          setXDir(0), setYDir(-1);
+        }}
+      >
+        <Text>UP</Text>
+      </Pressable>
       <View style={{ flexDirection: "row" }}>
-        <Pressable style={styles.directionButtons} onPress={() => setXDir(-1)}>
+        <Pressable
+          style={styles.directionButtons}
+          onPress={() => {
+            setYDir(0), setXDir(-1);
+          }}
+        >
           <Text>LEFT</Text>
         </Pressable>
-        <Pressable style={styles.directionButtons} onPress={() => setXDir(0)}>
+        <Pressable
+          style={styles.directionButtons}
+          onPress={() => {
+            setXDir(0), setYDir(0);
+          }}
+        >
           <Text>-0-</Text>
         </Pressable>
-        <Pressable style={styles.directionButtons} onPress={() => setXDir(1)}>
+        <Pressable
+          style={styles.directionButtons}
+          onPress={() => {
+            setYDir(0), setXDir(1);
+          }}
+        >
           <Text>RIGHT</Text>
         </Pressable>
       </View>
+      <Pressable
+        style={styles.directionButtons}
+        onPress={() => {
+          setXDir(0), setYDir(1);
+        }}
+      >
+        <Text>DOWN</Text>
+      </Pressable>
       <Pressable
         style={{ marginBottom: 20 }}
         // onPress={() => startGameHandler()}
@@ -186,11 +177,11 @@ export default function TabTwoScreen() {
         <Text>Start Game?</Text>
       </Pressable>
       {/* <Text>HELL OTHIS IS TAB TWO BUT WHERE IS MODAL?</Text> */}
-      <View>
+      <View style={{ flexDirection: "row" }}>
         {snakeField.map((row, rIndex) => {
           // return <Text>{row}</Text>;
           return (
-            <View key={rIndex} style={styles.oneRowInGame}>
+            <View key={rIndex}>
               {row.map((oneCell, index) => {
                 return (
                   <Text key={index} style={styles.oneTextInField}>
@@ -211,11 +202,16 @@ const styles = StyleSheet.create({
   // Snake
   oneRowInGame: {
     flex: 1,
-    flexDirection: "row",
+    // flexDirection: "row",
   },
   oneTextInField: {
-    padding: 20,
-    margin: 5,
+    // padding: 10,
+    alignItems: "center",
+    justifyContent: "center",
+    textAlign: "center",
+    height: 25,
+    width: 20,
+    margin: 3,
     borderWidth: 1,
   },
   directionButtons: {
